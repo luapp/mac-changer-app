6e00f9c7e14531765b87083c45828ae5b92fdde3
Paul Le Gall <darky.dev@outlook.com>
2024-09-15 01:53:57 +0200

    [ADD] file management basic functions and tauri scopes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
 import { useState, useEffect } from "react";
 import { mac_address_generator } from './Components/Logics/macAddressGenerator';
+import { accessSaveFile } from './Components/Logics/fileManagement';
 import styles from './App.module.css';
 
 const App = () => {
-    const [isOn, setIsOn] = useState(false); // For tracking the on/off state
-    const [loading, setLoading] = useState(false); // For tracking loading state
-    const [completedSteps, setCompletedSteps] = useState(0); // Track steps completed
-    const totalSteps = 4; // Dynamic total number of steps, adjust as needed
+    const [isOn, setIsOn] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [completedSteps, setCompletedSteps] = useState(0);
+    const totalSteps = 4;
 
     const asyncFunction01 = async () => {
-        const randomDelay = 1 // Random delay between 1s and 3s
+        const randomDelay = 1
         return new Promise(resolve => setTimeout(resolve, randomDelay));
     };
     const asyncFunction02 = async () => {
-        const randomDelay = 1 // Random delay between 1s and 3s
+        const randomDelay = 1
         return new Promise(resolve => setTimeout(resolve, randomDelay));
     };
     const asyncFunction03 = async () => {
-        const randomDelay = 1 // Random delay between 1s and 3s
+        const randomDelay = 1
         return new Promise(resolve => setTimeout(resolve, randomDelay));
     };
     const asyncFunction04 = async () => {
-        const randomDelay = 1 // Random delay between 1s and 3s
+        const randomDelay = 1
         return new Promise(resolve => setTimeout(resolve, randomDelay));
     };
+
     const executeSteps = async () => {
+        console.log("Steps initiated...");
+        if (isOn) {
+            await executeDeactivationSteps();
+        } else {
+            await executeActivationSteps();
+        }
+    }
+
+
+    const executeActivationSteps = async () => {
+        const status = await accessSaveFile();
+        if (status === undefined) {
+            console.error("Failed to access drive and create mac address backup...");
+            return;
+        }
+        console.log("Activation steps initiated...");
         setLoading(true);
         setCompletedSteps(0); // Reset step progress
         const stepFunctions = [asyncFunction01, asyncFunction02, asyncFunction03, asyncFunction04];
@@ -36,6 +54,19 @@ const App = () => {
         setLoading(false); // Set loading state to false when all steps are done
     };
     const loadingPercentage = Math.floor((completedSteps / totalSteps) * 100); // Calculate dynamic percentage
+
+    const executeDeactivationSteps = async () => {
+        console.log("Deactivation steps initiated...");
+        setLoading(true);
+        setCompletedSteps(0);
+        const stepFunctions = [asyncFunction04, asyncFunction03, asyncFunction02, asyncFunction01];
+        for (let i = 0; i < stepFunctions.length; i++) {
+            await stepFunctions[i]();
+            setCompletedSteps(i + 1);
+        }
+        setIsOn(prev => !prev);
+        setLoading(false);
+    }
 
     
     return (
--- src/Components/Logics/fileManagement.js
+++ src/Components/Logics/fileManagement.js
@@ -1,2 +1,106 @@
 import { exists, writeTextFile, createDir, BaseDirectory } from '@tauri-apps/api/fs';
+import { appDataDir, appDir } from '@tauri-apps/api/path';
+
+export const createLocalSaveDir = async () => {
+    try {
+        await createDir('', {dir: BaseDirectory.AppData, recursive: true });
+        return 0;
+    } catch (error) {
+        console.error('Failed to create directory:', error);
+        return undefined;
+    }
+}
+
+export const lookupLocalSaveDirPath = async () => {
+    try {
+        const appDataPath = await appDataDir();
+        return appDataPath;
+    } catch (error) {
+        console.error('Failed to get app data path:', error);
+        return undefined;
+    }
+}
+
+export const lookupSaveFile = async () => {
+    const appDataPath = await lookupLocalSaveDirPath();
+    const filePath = `${appDataPath}app_local_storage.dat`;
+    const directoryExists = await exists(filePath);
+    return directoryExists;
+}
+
+export const createSaveFile = async (filePath) => {
+    try {
+        const data = JSON.stringify({
+            macAddressBackupStatus: false,
+            macAddressBackup: 'null',
+        }, null, 4);
+        await writeTextFile(filePath, data);
+        return 0;
+    } catch (error) {
+        console.error('Failed to save object:', error);
+        return undefined;
+    }
+};
+
+export const readSaveFile = async (filePath) => {
+    try {
+        const data = await readTextFile(filePath);
+        const parsedData = JSON.parse(data);
+        return parsedData;
+    } catch (error) {
+        console.error('Failed to read object:', error);
+        return undefined;
+    }
+}
+
+export const writeOriginalMacAddressToSaveFile = async (filePath, parsedData) => {
+    try {
+        if (parsedData.macAddressBackupStatus === false) {
+            const currentMac = "00:00:00:00:00";
+            if (currentMac !== undefined) {
+                parsedData.macAddressBackup = currentMac;
+                parsedData.macAddressBackupStatus = true;
+                const newData = JSON.stringify(parsedData, null, 4);
+                await writeTextFile(filePath, newData);
+            }
+        }
+    }
+    catch (error) {
+        console.error('Failed to write object:', error);
+        return undefined;
+    }
+}
+
+
+export const accessSaveFile = async () => {
+    let savePathExists = await lookupLocalSaveDirPath();
+
+    if (savePathExists) {
+        const localSaveDirStatus = await createLocalSaveDir();
+        if (localSaveDirStatus === undefined) {
+            return undefined;
+        }
+        let saveFileExists = await lookupSaveFile();
+        if (!saveFileExists) {
+            const filePath = `${savePathExists}app_local_storage.dat`;
+            const saveFileStatus = await createSaveFile(filePath);
+            if (saveFileStatus === undefined) {
+                return undefined;
+            }
+        } else {
+            const saveReadStatus = await readSaveFile(`${savePathExists}app_local_storage.dat`);
+            if (saveReadStatus === undefined) {
+                return undefined;
+            }
+            const writeMacBackupStatus = await writeOriginalMacAddressToSaveFile(`${savePathExists}app_local_storage.dat`, saveReadStatus);
+            if (writeMacBackupStatus === undefined) {
+                return undefined;
+            }
+        }
+    } else {
+        return undefined;
+    }
+    return 0;
+}
+
 
