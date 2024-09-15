a0f444ac5734851256bcc8f775c55e3f6165a1c4
Paul Le Gall <darky.dev@outlook.com>
2024-09-15 06:36:22 +0200

    [FIX] refactor activation and deactivation code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
-import { macAddressGenerator, getCurrentMacAddress } from './Components/Logics/macAddressLogic';
-import { accessSaveFile, fetchOriginalMacAddressFromSaveFile } from './Components/Logics/fileManagement';
+import { getCurrentMacAddress } from './Components/Logics/macAddressLogic';
 import { checkOperatingSystem } from './Components/Logics/osCheck';
-import { fetchNetworkInterface, macAddressModifier } from './Components/Logics/networkInterfaceManagement';
+import { executeActivationSteps, executeDeactivationSteps } from './Components/Logics/toggle';
 import styles from './App.module.css';
 
 const App = () => {
@@ -10,144 +9,16 @@ const App = () => {
     const [loading, setLoading] = useState(false);
     const [completedSteps, setCompletedSteps] = useState(0);
     const [currentMacAddress, setCurrentMacAddress] = useState(undefined);
-    const totalSteps = 3;
 
     const executeSteps = async () => {
         console.log("Steps initiated...");
         if (isOn) {
-            await executeDeactivationSteps();
+            await executeDeactivationSteps({ setIsOn, setLoading, setCompletedSteps });
         } else {
-            await executeActivationSteps();
+            await executeActivationSteps({ setIsOn, setLoading, setCompletedSteps });
         }
     }
 
-
-    const executeActivationSteps = async () => {
-        console.log("Activation steps initiated...");
-        setLoading(true);
-        setCompletedSteps(0);
-        try {
-            let networkInterfaceStatus = undefined;
-
-            const stepFunctions = [
-                async () => {
-                    const accessStatus = await accessSaveFile();
-                    if (accessStatus === undefined) {
-                        console.error("Failed to access drive and create MAC address backup...");
-                        return false;
-                    }
-                    const originalMacAddress = await fetchOriginalMacAddressFromSaveFile(accessStatus);
-                    console.log(`Original MAC address: ${originalMacAddress}`);
-                    if (originalMacAddress === undefined) {
-                        console.error("Failed to fetch original MAC address...");
-                        return false;
-                    }
-                    return true;
-                },
-                async () => {
-                    networkInterfaceStatus = await fetchNetworkInterface();
-                    if (networkInterfaceStatus === undefined) {
-                        console.error("Failed to fetch network interface status...");
-                        return false;
-                    }
-                    return true;
-                },
-                async () => {
-                    let randomMacAddress = undefined;
-                    randomMacAddress = macAddressGenerator();
-                    if (randomMacAddress === undefined) {
-                        console.error("Failed to generate random MAC address...");
-                        return false;
-                    }
-                    const randomMacAddressInjectionStatus = await macAddressModifier(networkInterfaceStatus, randomMacAddress);
-                    if (randomMacAddressInjectionStatus === undefined) {
-                        console.error("Failed to inject random MAC address...");
-                        return false;
-                    }
-                    return true;
-                }
-            ];
-            for (let i = 0; i < stepFunctions.length; i++) {
-                const stepSuccess = await stepFunctions[i]();
-                if (!stepSuccess) {
-                    console.error(`Step ${i + 1} failed...`);
-                    setLoading(false);
-                    return;
-                }
-                setCompletedSteps(i + 1);
-            }
-            setIsOn(prev => !prev);
-            setLoading(false);
-        }
-        catch (error) {
-            console.error("An error occurred during the activation steps:", error);
-            setLoading(false);
-        }
-    };
-
-    const executeDeactivationSteps = async () => {
-        console.log("Deactivation steps initiated...");
-        setLoading(true);
-        setCompletedSteps(0);
-        try {
-            let networkInterfaceStatus = undefined;
-            let originalMacAddress = undefined;
-
-            const stepFunctions = [
-                async () => {
-                    const accessStatus = await accessSaveFile();
-                    if (accessStatus === undefined) {
-                        console.error("Failed to access drive and create MAC address backup...");
-                        return false;
-                    }
-                    originalMacAddress = await fetchOriginalMacAddressFromSaveFile(accessStatus);
-                    console.log(`Original MAC address: ${originalMacAddress}`);
-                    if (originalMacAddress === undefined) {
-                        console.error("Failed to fetch original MAC address...");
-                        return false;
-                    }
-                    return true;
-                },
-                async () => {
-                    networkInterfaceStatus = await fetchNetworkInterface();
-                    if (networkInterfaceStatus === undefined) {
-                        console.error("Failed to fetch network interface status...");
-                        return false;
-                    }
-                    return true;
-                },
-                async () => {
-                    let randomMacAddress = undefined;
-                    randomMacAddress = macAddressGenerator();
-                    if (randomMacAddress === undefined) {
-                        console.error("Failed to generate random MAC address...");
-                        return false;
-                    }
-                    const randomMacAddressInjectionStatus = await macAddressModifier(networkInterfaceStatus, originalMacAddress);
-                    if (randomMacAddressInjectionStatus === undefined) {
-                        console.error("Failed to inject random MAC address...");
-                        return false;
-                    }
-                    return true;
-                }
-            ];
-            for (let i = 0; i < stepFunctions.length; i++) {
-                const stepSuccess = await stepFunctions[i]();
-                if (!stepSuccess) {
-                    console.error(`Step ${i + 1} failed...`);
-                    setLoading(false);
-                    return;
-                }
-                setCompletedSteps(i + 1);
-            }
-            setIsOn(prev => !prev);
-            setLoading(false);
-        }
-        catch (error) {
-            console.error("An error occurred during the activation steps:", error);
-            setLoading(false);
-        }
-    }
 
     useEffect(() => {
         const fetchCurrentMacAddress = async () => {
---
+++ src/Components/Logics/toggle.js
@@ -0,0 +1,130 @@
+import { accessSaveFile, fetchOriginalMacAddressFromSaveFile } from "./fileManagement";
+import { fetchNetworkInterface, macAddressModifier } from "./networkInterfaceManagement";
+import { macAddressGenerator } from "./macAddressLogic";
+
+export const executeActivationSteps = async ({setIsOn, setLoading, setCompletedSteps}) => {
+    console.log("Activation steps initiated...");
+    setLoading(true);
+    setCompletedSteps(0);
+    try {
+        let networkInterfaceStatus = undefined;
+
+        const stepFunctions = [
+            async () => {
+                const accessStatus = await accessSaveFile();
+                if (accessStatus === undefined) {
+                    console.error("Failed to access drive and create MAC address backup...");
+                    return false;
+                }
+                const originalMacAddress = await fetchOriginalMacAddressFromSaveFile(accessStatus);
+                console.log(`Original MAC address: ${originalMacAddress}`);
+                if (originalMacAddress === undefined) {
+                    console.error("Failed to fetch original MAC address...");
+                    return false;
+                }
+                return true;
+            },
+            async () => {
+                networkInterfaceStatus = await fetchNetworkInterface();
+                if (networkInterfaceStatus === undefined) {
+                    console.error("Failed to fetch network interface status...");
+                    return false;
+                }
+                return true;
+            },
+            async () => {
+                let randomMacAddress = undefined;
+                randomMacAddress = macAddressGenerator();
+                if (randomMacAddress === undefined) {
+                    console.error("Failed to generate random MAC address...");
+                    return false;
+                }
+                const randomMacAddressInjectionStatus = await macAddressModifier(networkInterfaceStatus, randomMacAddress);
+                if (randomMacAddressInjectionStatus === undefined) {
+                    console.error("Failed to inject random MAC address...");
+                    return false;
+                }
+                return true;
+            }
+        ];
+        for (let i = 0; i < stepFunctions.length; i++) {
+            const stepSuccess = await stepFunctions[i]();
+            if (!stepSuccess) {
+                console.error(`Step ${i + 1} failed...`);
+                setLoading(false);
+                return;
+            }
+            setCompletedSteps(i + 1);
+        }
+        setIsOn(prev => !prev);
+        setLoading(false);
+    }
+    catch (error) {
+        console.error("An error occurred during the activation steps:", error);
+        setLoading(false);
+    }
+};
+
+export const executeDeactivationSteps = async ({setIsOn, setLoading, setCompletedSteps}) => {
+    console.log("Deactivation steps initiated...");
+    setLoading(true);
+    setCompletedSteps(0);
+    try {
+        let networkInterfaceStatus = undefined;
+        let originalMacAddress = undefined;
+
+        const stepFunctions = [
+            async () => {
+                const accessStatus = await accessSaveFile();
+                if (accessStatus === undefined) {
+                    console.error("Failed to access drive and create MAC address backup...");
+                    return false;
+                }
+                originalMacAddress = await fetchOriginalMacAddressFromSaveFile(accessStatus);
+                console.log(`Original MAC address: ${originalMacAddress}`);
+                if (originalMacAddress === undefined) {
+                    console.error("Failed to fetch original MAC address...");
+                    return false;
+                }
+                return true;
+            },
+            async () => {
+                networkInterfaceStatus = await fetchNetworkInterface();
+                if (networkInterfaceStatus === undefined) {
+                    console.error("Failed to fetch network interface status...");
+                    return false;
+                }
+                return true;
+            },
+            async () => {
+                let randomMacAddress = undefined;
+                randomMacAddress = macAddressGenerator();
+                if (randomMacAddress === undefined) {
+                    console.error("Failed to generate random MAC address...");
+                    return false;
+                }
+                const randomMacAddressInjectionStatus = await macAddressModifier(networkInterfaceStatus, originalMacAddress);
+                if (randomMacAddressInjectionStatus === undefined) {
+                    console.error("Failed to inject random MAC address...");
+                    return false;
+                }
+                return true;
+            }
+        ];
+        for (let i = 0; i < stepFunctions.length; i++) {
+            const stepSuccess = await stepFunctions[i]();
+            if (!stepSuccess) {
+                console.error(`Step ${i + 1} failed...`);
+                setLoading(false);
+                return;
+            }
+            setCompletedSteps(i + 1);
+        }
+        setIsOn(prev => !prev);
+        setLoading(false);
+    }
+    catch (error) {
+        console.error("An error occurred during the activation steps:", error);
+        setLoading(false);
+    }
+}
