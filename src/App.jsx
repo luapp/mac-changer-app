68846a1068b4cef6ce54102fa351230e874ce175
Paul Le Gall <darky.dev@outlook.com>
2024-09-16 12:18:42 +0200

    [ADD] settings menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { getCurrentMacAddress } from './Components/Logics/macAddressLogic';
 import { checkOperatingSystem } from './Components/Logics/osCheck';
+import { lookupSaveFile, lookupLocalSaveDirPath, fetchOriginalMacAddressFromSaveFile } from './Components/Logics/fileManagement';
 import { executeActivationSteps, executeDeactivationSteps } from './Components/Logics/toggle';
 import Header from "./Components/UI/Header";
 import styles from './App.module.css';
@@ -10,6 +11,7 @@ const App = () => {
     const [loading, setLoading] = useState(false);
     const [completedSteps, setCompletedSteps] = useState(0);
     const [currentMacAddress, setCurrentMacAddress] = useState(undefined);
+    const [manualMacAddress, setManualMacAddress] = useState(false);
 
     const executeSteps = async () => {
         let status = undefined;
@@ -32,7 +34,24 @@ const App = () => {
             const currentMac = await getCurrentMacAddress();
             setCurrentMacAddress(currentMac);
         }
+        const wasActive = async () => {
+            const currentMac = await getCurrentMacAddress();
+            let macFromSave = undefined;
+            let saveFilePath = undefined;
+            const isThereSaveFilePath = await lookupSaveFile();
+            if (isThereSaveFilePath === true) {
+                saveFilePath = await lookupLocalSaveDirPath();
+                if (saveFilePath === undefined) {
+                    return;
+                }
+                macFromSave = await fetchOriginalMacAddressFromSaveFile(`${saveFilePath}app_local_storage.dat`);
+            }
+            if (macFromSave.trim() !== currentMac.trim()) {
+                setIsOn(true);
+            }
+        }
         fetchCurrentMacAddress();
+        wasActive()
     }, [currentMacAddress]);
 
     useEffect(() => {
@@ -49,21 +68,26 @@ const App = () => {
     return (
         <div className={styles.app}>
             <Header />
-            <button
-                onClick={executeSteps}
-                disabled={loading}
-                className={`${styles.toggleButton} ${isOn ? styles.on : styles.off}`}
-                style={loading ? { '--completedSteps': completedSteps } : {}}
-            >
-                {
-                loading ? <span>&#x23FB;</span>
-                : isOn ? <span>&#x23FB;</span>
-                : <span>&#x23FB;</span>
-                }
-            </button>
-            <div className={styles.macAddress}>
-                <p>Current MAC Address:</p>
-                <p>{currentMacAddress}</p>
+            <div className={styles.main}>
+                <button
+                    onClick={executeSteps}
+                    disabled={loading}
+                    className={`${styles.toggleButton} ${isOn ? styles.on : styles.off}`}
+                    style={loading ? { '--completedSteps': completedSteps } : {}}
+                >
+                    {
+                    loading ? <span>&#x23FB;</span>
+                    : isOn ? <span>&#x23FB;</span>
+                    : <span>&#x23FB;</span>
+                    }
+                </button>
+                <div className={styles.macAddress}>
+                    <p className={styles.textInfo}>Current MAC Address:</p>
+                    <p className={styles.textInfo}>{currentMacAddress}</p>
+                </div>
+                <div className={styles.manualMacAddress}>
+                    <p className={styles.textInfo}>Manual input: {manualMacAddress.toString()}</p>
+                </div>
             </div>
         </div>
     );
--- src/Components/UI/Header.jsx
+++ src/Components/UI/Header.jsx
@@ -1,13 +1,48 @@
 import styles from './header.module.css';
-import line from './line.png';
+import { useState, useEffect } from 'react';
+import { Squash as Hamburger } from 'hamburger-react';
 
 const Header = () => {
+    const [isSettingsOpen, setSettingsOpen] = useState(false);
+    const [title, setTitle] = useState("");
+
+    useEffect(() => {
+        if (isSettingsOpen) {
+            setTitle("Settings");
+        } else {
+            setTitle("");
+        }
+    }, [isSettingsOpen]);
+
     return (
         <header className={styles.header}>
-            <img className={styles.img} src={line} alt='...'/>
+            <div className={styles.navButton}>
+                <Hamburger
+                    toggled={isSettingsOpen}
+                    toggle={setSettingsOpen}
+                    duration={0.3}
+                    size={32}
+                    color="#FFFFFF"
+                />
+            </div>
+            <h2 className={styles.title}>{title}</h2>
+            {isSettingsOpen && <Settings />}
         </header>
     );
 }
 
+const Settings = () => {
+    return (
+        <div className={styles.settings}>
+            <ul className={styles.settingList}>
+                <li>Manual MAC address input</li>
+                <li>Check Original MAC address</li>
+                <li>Contact Us</li>
+                <br/>
+                <li>This is a Beta version</li>
+            </ul>
+        </div>
+    );
+}
 
 export default Header;
