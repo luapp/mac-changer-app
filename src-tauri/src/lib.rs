86b2fe5d188bc23134955d4d8d07564bd133b34c
Paul Le Gall <paul.le-gall@outlook.com>
2024-12-24 02:13:27 +0100

    [ADD] getAllNetworkInterfaces from macos shell

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,47 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+/*
+* © 2024 Paul Le Gall. All Rights Reserved.
+* This code is proprietary and confidential. Unauthorized copying, reproduction, or redistribution is strictly prohibited.
+*/
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_all_network_interfaces() -> Result<String, String> {
+    let output = std::process::Command::new("networksetup")
+        .arg("-listallhardwareports")
+        .output()
+        .expect("failed to execute process");
+    let output = String::from_utf8(output.stdout);
+    match output {
+        Ok(output) => {
+            let output = output.split("\n");
+            let mut interfaces = Vec::new();
+            for line in output {
+                if line.contains("Hardware Port:") {
+                    let interface = line.split("Hardware Port:").collect::<Vec<&str>>()[1].trim();
+                    let interface = interface.split("(").collect::<Vec<&str>>()[0].trim();
+                    interfaces.push(interface);
+                }
+                if line.contains("Device:") {
+                    let interface = line.split("Device:").collect::<Vec<&str>>()[1].trim();
+                    interfaces.push(interface);
+                }
+                if line.contains("Ethernet Address:") {
+                    let interface = line.split("Ethernet Address:").collect::<Vec<&str>>()[1].trim();
+                    interfaces.push(interface);
+                }
+            }
+            Ok(interfaces.join("\n"))
+        }
+        Err(e) => Err(e.to_string()),
+    }
 }
+
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_network_interfaces])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,33 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+/*
+* © 2024 Paul Le Gall. All Rights Reserved.
+* This code is proprietary and confidential. Unauthorized copying, reproduction, or redistribution is strictly prohibited.
+*/
+
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    async function getAllNetworkInterfaces() {
+        const interfaces = await invoke("get_all_network_interfaces");
+        const lines = interfaces.split("\n").filter((line) => line.trim() !== "");
+        const interfacesObject = {};
+      
+        for (let i = 0; i < lines.length; i += 3) {
+            const key = Math.floor(i / 3);
+            interfacesObject[key] = [
+                lines[i] || null,
+                lines[i + 1] || null,
+                lines[i + 2] || null
+            ];
+        }
+        console.log(interfacesObject);
+    }
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+        <button onClick={getAllNetworkInterfaces}>Button</button>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
     </main>
   );
 }
