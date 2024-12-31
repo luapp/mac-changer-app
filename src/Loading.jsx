93e3500e8c5ddeb7286d3fb398bcd75e12afe3a2
Paul Le Gall <paul.le-gall@outlook.com>
2024-12-31 23:52:38 +0100

    [ADD] OS filter to app

---
+++ src/Loading.jsx
@@ -0,0 +1,14 @@
+/*
+* © 2024 Paul Le Gall. All Rights Reserved.
+* This code is proprietary and confidential. Unauthorized copying, reproduction, or redistribution is strictly prohibited.
+*/
+
+const Loading = () => {
+    return (
+        <div>
+            <h1>App is loading...</h1>
+        </div>
+    );
+}
+
+export default Loading;
--- src/router.jsx
+++ src/router.jsx
@@ -6,9 +6,12 @@
 import { invoke } from "@tauri-apps/api/core";
 import { useState, useEffect } from "react";
 import App from "./App"
+import Loading from "./Loading";
+import {getOs} from "./logic/os";
 
 const Router = () => {
     const [interfacesObjectState, setInterfacesObjectState] = useState({});
+    const [currentOs, setCurrentOs] = useState(undefined);
     
 
     async function getAllNetworkInterfaces() {
@@ -36,12 +39,29 @@ const Router = () => {
     }
 
     useEffect(() => {
-        getAllWifiInterfaces();
+        getOs().then((osResult) => {
+            setCurrentOs(osResult);
+        });
+        //getAllWifiInterfaces();
     }, []);
 
-    return (
-        <App />
-    );
+    if (currentOs === undefined) {
+        return (
+            <Loading />
+        )
+    } else {
+        if (currentOs !== "macos") {
+            return (
+                <div>
+                    <h1>Sorry, this app is only available on macOS</h1>
+                </div>
+            );
+        } else {
+            return (
+                <App />
+            );
+        }
+    }
 }
 
 export default Router;
