7bf1e60d0d19258b28fda47fc64731049022e1c2
Paul Le Gall <paul.le-gall@outlook.com>
2025-01-04 20:49:46 +0100

    [ADD] header setting icon

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,19 @@
-import styles from "./Header.module.css"
+/*
+* © 2024 Paul Le Gall. All Rights Reserved.
+* This code is proprietary and confidential. Unauthorized copying, reproduction, or redistribution is strictly prohibited.
+*/
 
-const Header = () => {
+import styles from "./Header.module.css"
+import gearShapeEmptySf10 from "./resources/images/gearShapeEmptySf10.png"
+import gearShapeFillSf10 from "./resources/images/gearShapeFillSf10.png"
+
+const Header = ({isSettingsOpen,setIsSettingsOpen}) => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Mac Changer</h1>
-            <h1 className={styles.settings}>Settings</h1>
+            <div className={styles.settings}>
+                <img src={gearShapeFillSf10} className={styles.settingsGearIcon} onClick={() => setIsSettingsOpen(!isSettingsOpen)}></img>
+            </div>
         </div>
     );
 }
