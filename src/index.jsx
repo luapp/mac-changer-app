9073264a554d35b00e0c851bc8e4fac0cbac6353
Paul Le Gall <paul.le-gall@outlook.com>
2025-01-04 20:50:10 +0100

    [ADD] index header

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,8 @@
+/*
+* © 2024 Paul Le Gall. All Rights Reserved.
+* This code is proprietary and confidential. Unauthorized copying, reproduction, or redistribution is strictly prohibited.
+*/
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./router";
