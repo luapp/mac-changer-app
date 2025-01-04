67920e857fde1beae3d5ab309e6335e4c67e7ba4
Paul Le Gall <paul.le-gall@outlook.com>
2025-01-04 20:51:21 +0100

    [ADD] settings menu to main app

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,24 @@ import Header from "./Header"
 import {useState, useEffect} from "react"
 import {getAllNetworkInterfaces, getAllWifiInterfaces} from "./logic/network"
 import Toggle from "./Toggle"
+import Settings from "./Settings"
+
+import { invoke } from "@tauri-apps/api/core";
 
 const App = () => {
 
+    const randomtime = async () => {
+        setIsLoading(true);
+        let res = await invoke("random_time_perso");
+        setIsLoading(false);
+        console.log(res);
+    }
+
     const [currentAllNetworkInterfaces, setCurrentAllNetworkInterfaces] = useState({});
     const [currentAllWifiInterfaces, setCurrentAllWifiInterfaces] = useState(undefined);
+    const [toggleUpdate, setToggleUpdate] = useState(undefined);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isSettingsOpen, setIsSettingsOpen] = useState(false);
 
     useEffect(() => {
         getAllNetworkInterfaces().then((interfacesObject) => {
@@ -25,26 +38,41 @@ const App = () => {
         });
     }, []);
 
+    useEffect(() => {
+        if (toggleUpdate !== undefined) {
+            console.log("toggleUpdate", toggleUpdate);
+            randomtime();
+        }
+    }, [toggleUpdate]);
+
     return (
         <div className={styles.container}>
-            <Header />
-            <div className={styles.appContent}>
-                <div className={styles.toggle}>
-                    <Toggle isOn={true} onToggle={(state) => console.log('New state:', state)} scale={3.8}/>
+            <Header isSettingsOpen={isSettingsOpen} setIsSettingsOpen={setIsSettingsOpen}/>
+                <div className={styles.appContent}>
+                    <div className={styles.toggle}>
+                        <Toggle isOn={true} onToggle={(state) => setToggleUpdate(state)} scale={3.8} disabled={isLoading}/>
+                    </div>
+                    <p>
+                        {currentAllWifiInterfaces?.[0]?.[0]
+                            ? `Default ${currentAllWifiInterfaces[0][0]} device selected`
+                            : "Loading default network interface"
+                        }
+                    </p>
+                    <p>
+                        {currentAllWifiInterfaces?.[0]?.[1]
+                            ? `Original macaddress: ${currentAllWifiInterfaces[0][1]}`
+                            : "Loading original macaddress"
+                        }
+                    </p>
+                    {isLoading && (
+                        <div className={styles.spinner}>
+                            <div className={styles.loader}></div>
+                        </div>
+                    )}
                 </div>
-                <p>
-                    {currentAllWifiInterfaces?.[0]?.[0]
-                        ? `Default ${currentAllWifiInterfaces[0][0]} device selected`
-                        : "Loading default network interface"
-                    }
-                </p>
-                <p>
-                    {currentAllWifiInterfaces?.[0]?.[1]
-                        ? `Original macaddress: ${currentAllWifiInterfaces[0][1]}`
-                        : "Loading original macaddress"
-                    }
-                </p>
-            </div>
+                {isSettingsOpen && (
+                    <Settings />
+                )}
         </div>
     );
 }
