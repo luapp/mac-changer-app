37b1128dd7b548e75914324f213fa1c6148ce6f5
Paul Le Gall <paul.le-gall@outlook.com>
2025-01-04 23:55:47 +0100

    [ADD] all settings sub pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import {useState, useEffect} from "react"
 import {getAllNetworkInterfaces, getAllWifiInterfaces} from "./logic/network"
 import Toggle from "./Toggle"
 import Settings from "./components/Settings"
-
 import { invoke } from "@tauri-apps/api/core";
 
 const App = () => {
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -4,22 +4,88 @@
 */
 
 
+import { useState } from "react";
 import styles from "./Settings.module.css";
 
-const Settings = () => {
+const SettingsHome = ({setCurrentSettings}) => {
+
+    const handleSettingsChange = (newSettingsState) => {
+        setCurrentSettings(newSettingsState);
+    }
+
+    return (
+        <ul className={styles.settingsList}>
+            <li className={styles.settingsListElement} onClick={() => handleSettingsChange("General")}>General</li>
+            <li className={styles.settingsListElement} onClick={() => handleSettingsChange("Interface Configuration")}>Interface Configuration</li>
+            <li className={styles.settingsListElement} onClick={() => handleSettingsChange("MAC Address Settings")}>MAC Address Settings</li>
+            <li className={styles.settingsListElement} onClick={() => handleSettingsChange("Help & Support")}>Help & Support</li>
+            <li className={styles.settingsListElement} onClick={() => handleSettingsChange("About")}>About</li>
+        </ul>
+    );
+}
+
+const SettingsGeneral = () => {
+    return (
+        <div>
+            <p>general</p>
+        </div>
+    );
+}
+
+const SettingsInterfaceConfiguration = () => {
+    return (
+        <div>
+            <p>SettingsInterfaceConfiguration</p>
+        </div>
+    );
+}
+
+const SettingsMACAddressSettings = () => {
+    return (
+        <div>
+            <p>SettingsMACAddressSettings</p>
+        </div>
+    );
+}
+
+const SettingsHelpSupport = () => {
+    return (
+        <div>
+            <p>SettingsHelpSupport</p>
+        </div>
+    );
+}
+
+const SettingsAbout = () => {
     return (
         <div className={styles.settingsFixedOverlay}>
-        <div className={styles.settingsContent}>
-            <h1 className={styles.settingsTitleText}>Settings</h1>
-            <ul className={styles.settingsList}>
-                <li className={styles.settingsListElement}>General</li>
-                <li className={styles.settingsListElement}>Interface Configuration</li>
-                <li className={styles.settingsListElement}>MAC Address Settings</li>
-                <li className={styles.settingsListElement}>Help & Support</li>
-                <li className={styles.settingsListElement}>About</li>
-            </ul>
+            <div className={styles.settingsContent}>
+                <h1 className={styles.settingsTitleText}>About</h1>
+                <ul className={styles.settingsList}>
+                </ul>
+            </div>
         </div>
-    </div>
+    );
+}
+
+const Settings = () => {
+
+    const [currentSettings, setCurrentSettings] = useState("Settings");
+
+
+
+    return (
+        <div className={styles.settingsFixedOverlay}>
+            <div className={styles.settingsContent}>
+                <h1 className={styles.settingsTitleText}>{currentSettings}</h1>
+                {currentSettings === "Settings" && <SettingsHome setCurrentSettings={setCurrentSettings} />}
+                {currentSettings === "General" && <SettingsGeneral />}
+                {currentSettings === "Interface Configuration" && <SettingsInterfaceConfiguration />}
+                {currentSettings === "MAC Address Settings" && <SettingsMACAddressSettings />}
+                {currentSettings === "Help & Support" && <SettingsHelpSupport />}
+                {currentSettings === "About" && <SettingsAbout />}
+            </div>
+        </div>
     );
 }
 
